9e5d1330444919a11dd2452e3dc95ffac7afc11e
Mean Rose O Laplano <laplanomean55@gmail.com>
2026-09-10 08:11:54 +0800

    Update base URL for Render deployment

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['environment'] = getenv('APP_ENV') ?: 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url']  = 'http://localhost/lavalust/LavaLust/';
+$config['base_url'] = 'https://laplano-mean.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
